b58d1f89810f2141d2d8f6b9597d952dbbefad87
Bibiana.Sebestianova <17590227+Sebi2909@users.noreply.github.com>
2021-11-17 17:03:25 +0100

    feat: Search Books - controlled input is working when searching input - adapted css

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -9,7 +9,9 @@ const Book = ({ book }) => {
           style={{
             width: 128,
             height: 193,
-            backgroundImage: `url("${book.imageLinks.smallThumbnail}")`,
+            backgroundImage: book.imageLinks
+              ? `url("${book.imageLinks.thumbnail}")`
+              : null,
           }}
         />
         <div className="book-shelf-changer">
@@ -25,7 +27,9 @@ const Book = ({ book }) => {
         </div>
       </div>
       <div className="book-title">{book.title}</div>
-      <div className="book-authors">{book.authors.join(",")}</div>
+      <div className="book-authors">
+        {book.authors && book.authors.join(",")}
+      </div>
     </div>
   );
 };
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import * as BooksAPI from "../api/BooksAPI";
+import BookList from "../components/BookList";
 
 export const SearchPage = () => {
+  const [searchedBooks, setSearchedBooks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const onChangeInput = (value) => {
+    setInputValue(value);
+    BooksAPI.search(value).then((books) => {
+      if (books.error) {
+        setSearchedBooks([]);
+        return;
+      }
+      setSearchedBooks(books);
+    });
+  };
   return (
     <div className="search-books">
       <div className="search-books-bar">
         <Link className="close-search" to="/">
           Close
         </Link>
+        <input
+          type="text"
+          placeholder="Search by title or author"
+          value={inputValue}
+          onChange={(event) => onChangeInput(event.target.value)}
+        />
+      </div>
+      <div className="search-books-input-wrapper">
+        {searchedBooks.length > 0 && <BookList books={searchedBooks} />}
 
-        <div className="search-books-input-wrapper">
-          {/*
+        {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
               You can find these search terms here:
               https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -18,8 +41,6 @@ export const SearchPage = () => {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-          <input type="text" placeholder="Search by title or author" />
-        </div>
       </div>
       <div className="search-books-results">
         <ol className="books-grid" />
